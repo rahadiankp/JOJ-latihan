82dd9f042c6e11c72a42ae9c32899edd450122e9
Rahadian K. P <rahadiankp@users.noreply.github.com>
2015-10-30 18:05:05 +0700

    solve di mandi.cpp
    solve bro

--- mandi.cpp
+++ mandi.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
 int main() {
-	int T;
-	cin>>T;
-	for(int i=1; i<=T; i++) {
-		int N, M, L;
-		cin>>N>>M>>L;
-		int besar, kecil;
-		if(N>M) { besar = N; kecil = M; }
-		else {besar = M; kecil = N; }
-		while(L>0) {
-			L-=besar;
-			if(L%kecil==0) L = 0;
-		}
-		cout << "Kasus #" << i << ": ";
-		if(L==0) cout << "YA" << endl;
-		else cout << "TIDAK" << endl;
-	}
-	return 0;
+        int T;
+        cin>>T;
+        for(int i=1; i<=T; i++) {
+                int N, M, L;
+                cin>>N>>M>>L;
+                int besar, kecil;
+                if(N>M) { besar = N; kecil = M; }
+                else {besar = M; kecil = N; }
+                while(L>0) {
+                        if(L%besar==0) { L = 0; break; }
+                        L-=kecil;
+                }
+                cout << "Kasus #" << i << ": ";
+                if(L==0) cout << "YA" << endl;
+                else cout << "TIDAK" << endl;
+        }
+        return 0;
 }
